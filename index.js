e5b91e87a1b44c4766cab074e4567dd499deaff3
Mo Ansari <paytakhtdota@gmail.com>
2023-11-13 00:31:20 -0800

    all tasks done!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const input = document.querySelector("#inputNumber");
 const addNumberBTN = document.querySelector("#addNumber");
 const numbersBankOutput = document.querySelector("#numberBank output");
 const sortAllBTN = document.querySelector("#sortAll");
+const sortOneBTN = document.querySelector("#sortOne");
 const oddsOutput = document.querySelector("#odds output");
 const evensOutput = document.querySelector("#evens output");
 let numbers = [];
@@ -12,29 +13,50 @@ let evens = [];
 
 addNumberBTN.addEventListener("click", function (event) {
     event.preventDefault();
-    numbers.push(input.value);
+    const newInput = new Number(input.value);
+    numbers.push(newInput);
     console.log(numbers);
     numbersBankOutput.textContent = numbers.join(" , ");
-    input.value="";
+    input.value = "";
 });
 
 sortAllBTN.addEventListener("click", function (event) {
     event.preventDefault();
     const finder = numbers.forEach((number) => {
+
         if (number % 2 == 1) {
             odds.push(number)
             oddsOutput.textContent = odds.join(" , ");
         }
-        else {
+        else if (number % 2 == 0) {
             evens.push(number);
             evensOutput.textContent = evens.join(" , ");
         }
-        numbersBankOutput.textContent="";
+        else{
+        alert(`${number} is neither even nor odd!`);}
     });
-    console.log(evens);
-    console.log(odds);
-
+    numbersBankOutput.textContent = null;
+    numbers.length = 0;
 });
+
+sortOneBTN.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (numbers[0] % 2 == 1) {
+        odds.push(numbers[0])
+        oddsOutput.textContent = odds.join(" , ");
+    }
+    else {
+        evens.push(numbers[0]);
+        evensOutput.textContent = evens.join(" , ");
+    }
+
+
+    numbers.shift();
+    numbersBankOutput.textContent = numbers.join(" , ");
+});
+
+
+
 
 
 
